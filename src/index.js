697d6987fd4bf659063d9b04a2bc9d3a5a177fb1
Toad169 <mamangpunten893@gmail.com>
2025-10-21 09:34:03 +0700

    added auth routes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 
+import authRoutes from "./routes/auth.js";
+
 dotenv.config();
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => res.send("API is running..."));
 
